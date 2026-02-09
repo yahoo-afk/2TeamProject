6b63719c03bc5a82f60684bf1502d8f34ab62fcf
yahoo-afk <sjffhdfg@gmail.com>
2026-02-09 17:34:13 +0900

    git setting
    기본 파일노에셋 체크용

---
+++ Source/Team2Project/Team2Project.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Team2Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Team2Project, "Team2Project" );
